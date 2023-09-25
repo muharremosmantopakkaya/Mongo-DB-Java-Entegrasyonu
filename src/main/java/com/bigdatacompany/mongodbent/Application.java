37de25d12b794f9d6ad666ba1b218879f494cdbe
muharremosmantopakkaya <95377072+muharremosmantopakkaya@users.noreply.github.com>
2023-09-25 21:50:20 +0300

    MongoDB- Verilerin tarihini değiştirme

--- src/main/java/com/bigdatacompany/mongodbent/Application.java
+++ src/main/java/com/bigdatacompany/mongodbent/Application.java
@@ -2,9 +2,13 @@ package com.bigdatacompany.mongodbent;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.Arrays;
 
@@ -12,7 +16,7 @@ public class Application {
     public static void main(String[] args){
         MongoClient client =new MongoClient("localhost",27017);
         MongoDatabase infoDB = client.getDatabase("Info");
-       MongoCollection<Document> personelCollection = infoDB.getCollection("Personal");
+        MongoCollection<Document> personelCollection = infoDB.getCollection("Personal");
 
         BasicDBObject data=new BasicDBObject()
                 .append("name","Stephen Hawking")
@@ -24,8 +28,18 @@ public class Application {
                 .append("date",1643)
                 .append("country","England");
         //personelCollection.insertOne(Document.parse(data.toJson()));
-        Document parse =Document.parse(data.toJson());
+        /*Document parse =Document.parse(data.toJson());
         Document parse2 =Document.parse(data2.toJson());
         personelCollection.insertMany(Arrays.asList(parse,parse2));
+
+        FindIterable<Document> documents = personelCollection.find(new BasicDBObject("date",1942));
+        for (Document doc:documents){
+            System.out.println(doc.toJson());
+        } */
+
+        Bson filter= Filters.eq("name","Stephen Hawking");
+        Bson update= Updates.set("date",1952);
+        personelCollection.updateOne(filter,update);
+
     }
 }
