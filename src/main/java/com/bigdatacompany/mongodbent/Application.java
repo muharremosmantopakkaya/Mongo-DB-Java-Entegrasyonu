4d94dbeead6c7a23db2aa4b06ba0c5218eca9c12
muharremosmantopakkaya <95377072+muharremosmantopakkaya@users.noreply.github.com>
2023-09-25 21:34:13 +0300

    MongoDB- Birden fazla veri ekleme

--- src/main/java/com/bigdatacompany/mongodbent/Application.java
+++ src/main/java/com/bigdatacompany/mongodbent/Application.java
@@ -6,6 +6,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args){
         MongoClient client =new MongoClient("localhost",27017);
@@ -17,6 +19,13 @@ public class Application {
                 .append("date",1942)
                 .append("country","England");
 
-        personelCollection.insertOne(Document.parse(data.toJson()));
+        BasicDBObject data2=new BasicDBObject()
+                .append("name","Isaac Newton")
+                .append("date",1643)
+                .append("country","England");
+        //personelCollection.insertOne(Document.parse(data.toJson()));
+        Document parse =Document.parse(data.toJson());
+        Document parse2 =Document.parse(data2.toJson());
+        personelCollection.insertMany(Arrays.asList(parse,parse2));
     }
 }
